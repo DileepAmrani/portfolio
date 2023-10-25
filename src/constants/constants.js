b4fb11bd644f9613d52c80d122e45a46d858026f
DileepAmrani <amranidileep44@gmail.com>
2023-10-25 18:02:27 +0500

    update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,55 +1,79 @@
-export const projects = [{
-        title: "Gneeks",
-        description: "The #Gneeks is a market place where you can purchase diffrent types of NFT tokens and comics.",
-        image: "/images/1.png",
-        tags: ["Mongo", "Express", "React", "Node"],
-        source: "",
-        visit: "https://www.gneeks.org/",
-        id: 0,
-    },
-    {
-        title: "Pensioners Management System",
-        description: "This was my freelance Project, Two role based users, admin and user. Data can be send by admin using Excel Sheet. Advance Filtering System",
-        image: "/images/2.png",
-        tags: ["React JS", "Node JS", "Express", "MongoDB"],
-        source: "",
-        visit: "https://pensioners.netlify.app/",
-        id: 1,
-    },
+export const projects = [
+  {
+    title: "Gneeks",
+    description:
+      "The #Gneeks is a market place where you can purchase diffrent types of NFT tokens and comics.",
+    image: "/images/1.png",
+    tags: ["Mongo", "Express", "React", "Node"],
+    source: "",
+    visit: "https://www.gneeks.org/",
+    id: 0,
+  },
+  {
+    title: "Pensioners Management System",
+    description:
+      "This was my freelance Project, Two role based users, admin and user. Data can be send by admin using Excel Sheet. Advance Filtering System",
+    image: "/images/2.png",
+    tags: ["React JS", "Node JS", "Express", "MongoDB"],
+    source: "",
+    visit: "https://pensioners.netlify.app/",
+    id: 1,
+  },
+  {
+    title: "TARJETA ALIANZA (Coupon Website)",
+    description:
+      "I have developed a dynamic website using the MERN (MongoDB, Express, React, Node.js) stack. This website offers robust functionality for creating and managing coupons. Users can effortlessly generate new coupons and conveniently approve existing ones, streamlining the process of offering discounts and promotions.",
+    image: "/images/5.PNG",
+    tags: ["React JS", "Node JS", "Express", "MongoDB"],
+    source: "",
+    visit: "https://am-coupon-pr-7336feb2b38d.herokuapp.com/",
+    id: 2,
+  },
+  {
+    title: "Mixy Beauty",
+    description:
+      "I've crafted a stunning beauty website utilizing Next.js for a seamless, lightning-fast user experience. With the sleek and modern design powered by Tailwind CSS, this website offers a visually appealing platform for all things beauty-related. Explore the latest trends, beauty tips, and product recommendations.",
+    image: "/images/6.PNG",
+    tags: ["Next JS", "Tailwind Css"],
+    source: "",
+    visit: "https://amrani-mixybeauty.netlify.app/",
+    id: 3,
+  },
 ];
-export const skills = [{
-        image: "/images/html.png",
-    },
-    {
-        image: "/images/css.png",
-    },
-    {
-        image: "/images/js.png",
-    },
-    {
-        image: "/images/react.png",
-    },
-    {
-        image: "/images/vue.png",
-    },
-    {
-        image: "/images/firebase.png",
-    },
-    {
-        image: "/images/nodejs.png",
-    },
-    {
-        image: "/images/express.png",
-    },
-    {
-        image: "/images/mongodb.png",
-    },
+export const skills = [
+  {
+    image: "/images/html.png",
+  },
+  {
+    image: "/images/css.png",
+  },
+  {
+    image: "/images/js.png",
+  },
+  {
+    image: "/images/react.png",
+  },
+  {
+    image: "/images/vue.png",
+  },
+  {
+    image: "/images/firebase.png",
+  },
+  {
+    image: "/images/nodejs.png",
+  },
+  {
+    image: "/images/express.png",
+  },
+  {
+    image: "/images/mongodb.png",
+  },
 ];
 export const TimeLineData = [
-    { year: 2018, text: "Started my journey" },
-    { year: 2019, text: "Complete Learning Front-end" },
-    { year: 2020, text: "Worked as a freelancer" },
-    { year: 2020, text: "Started Learning Back-end" },
-    { year: 2021, text: "Worked as a Full Stack Developer" },
-    { year: 2023, text: "Complete 100+ projects" },
+  { year: 2018, text: "Started my journey" },
+  { year: 2019, text: "Complete Learning Front-end" },
+  { year: 2020, text: "Worked as a freelancer" },
+  { year: 2020, text: "Started Learning Back-end" },
+  { year: 2021, text: "Worked as a Full Stack Developer" },
+  { year: 2023, text: "Complete 100+ projects" },
 ];
